6e1dfabd6c45b3c05ee50a729589d71006ffe3a4
parak <parak@softserveinc.com>
2015-04-02 16:27:51 +0300

    - Created empty project.

---
+++ WebTextScanner/WebTextScanner/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  WebTextScanner
+//
+//  Created by parak on 4/2/15.
+//  Copyright (c) 2015 parak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ WebTextScanner/WebTextScanner/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  WebTextScanner
+//
+//  Created by parak on 4/2/15.
+//  Copyright (c) 2015 parak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
+
